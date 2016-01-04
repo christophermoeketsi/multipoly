fc76c004059963a3367e41b0e87ac70c35a326aa
christophermoeketsi <moeketsi.christopher@gmail.com>
2016-01-04 06:54:50 +0200

    new png and dice

--- m-web/src/main/resources/js/pages/landingPage.js
+++ m-web/src/main/resources/js/pages/landingPage.js
@@ -42,17 +42,169 @@ main.directive('leftLandDirective', function () {
         restrict: 'AE',
         replace: 'true',
         templateUrl: 'directives/leftLandDirective.html',
+        scope: {
+            streetName: '@',
+            cssClass: '@',
+            numberOfLayers: '@',
+            price: '@'
+        },
         link: function (scope, elem, attrs) {
-            elem.bind('click', function () {
-                elem.css('background-color', 'white');
-                scope.$apply(function () {
-                    scope.color = "white";
-                });
-            });
-            /*This should show the assets that are in that spot ??*/
-            elem.bind('mouseover', function () {
-                elem.css('cursor', 'pointer');
-            });
+        }
+    };
+});
+
+
+main.directive('leftPlain', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/electric.html',
+        scope: {
+            streetName: '@',
+            cssClass: '@',
+            numberOfLayers: '@',
+            price: '@'
+        },
+        link: function (scope, elem, attrs) {
+        }
+    };
+});
+
+
+main.directive('rightPlain', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/rightPlain.html',
+        scope: {
+            streetName: '@',
+            cssClass: '@',
+            numberOfLayers: '@',
+            price: '@'
+        },
+        link: function (scope, elem, attrs) {
+        }
+    };
+});
+
+
+main.directive('conner', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/conner.html',
+        scope: {
+            cssClass: '@'
+        },
+        link: function (scope, elem, attrs) {
+        }
+    };
+});
+
+
+main.directive('rightSimple', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/rightSimple.html',
+        scope: {
+            cssClass: '@',
+            displayText: '@'
+        },
+        link: function (scope, elem, attrs) {
+        }
+    };
+});
+
+
+main.directive('southLandDirective', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/southLandDirective.html',
+        scope: {
+            streetName: '@',
+            cssClass: '@',
+            numberOfLayers: '@',
+            price: '@'
+        },
+        link: function (scope, elem, attrs) {
+        }
+    };
+});
+
+
+main.directive('northLandDirective', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/northLandDirective.html',
+        scope: {
+            streetName: '@',
+            cssClass: '@',
+            numberOfLayers: '@',
+            price: '@'
+        },
+        link: function (scope, elem, attrs) {
+        }
+    };
+});
+
+
+main.directive('incomeTaxDirective', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/southLandDirective.html',
+        scope: {
+            cssClass: '@',
+            price: '@'
+        },
+        link: function (scope, elem, attrs) {
+        }
+    };
+});
+
+
+main.directive('plainDirective', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/plianDirective.html',
+        scope: {
+            cssClass: '@',
+            price: '@'
+        },
+        link: function (scope, elem, attrs) {
+        }
+    };
+});
+
+main.directive('chance', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/chance.html',
+        scope: {
+            cssClass: '@'
+        },
+        link: function (scope, elem, attrs) {
+        }
+    };
+});
+
+main.directive('rightLandDirective', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/rightDirective.html',
+        scope: {
+            streetName: '@',
+            cssClass: '@',
+            numberOfLayers: '@',
+            price: '@'
+        },
+        link: function (scope, elem, attrs) {
         }
     };
 });
@@ -65,7 +217,7 @@ main.directive('score', function () {
         templateUrl: 'directives/scoreDirective.html',
         scope: {
             amount: '@',
-            amountTitle:'@'
+            amountTitle: '@'
         },
         link: function (scope, elem, attrs) {
         }
@@ -80,9 +232,60 @@ main.directive('ranking', function () {
         templateUrl: 'directives/rankingDirective.html',
         scope: {
             rank: '@',
-            rankingStars :'@'
+            rankingStars: '@'
         },
         link: function (scope, elem, attrs) {
+        }
+    };
+});
+
+
+
+main.directive('chipDirective', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/chipDirective.html',
+        scope: {
+            chipId: '@',
+            cssClass: '@'
+        },
+        link: function (scope, elem, attrs) {
+        }
+    };
+});
+
+
+main.directive('dice', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        templateUrl: 'directives/dice.html',
+        scope: {
+
+        },
+        link: function (scope, elem, attrs) {
+            $(function (){
+                var dice = $("#dice");
+                dice.click(function () {
+                    $(".wrap").append("<div id='dice_mask'></div>");//add mask
+                    dice.attr("class", "dice");//After clearing the last points animation
+                    dice.css('cursor', 'default');
+                    var num = Math.floor(Math.random() * 6 + 1);//random num 1-6
+                    dice.animate({left: '+2px'}, 100, function () {
+                        dice.addClass("dice_t");
+                    }).delay(200).animate({top: '-2px'}, 100, function () {
+                        dice.removeClass("dice_t").addClass("dice_s");
+                    }).delay(200).animate({opacity: 'show'}, 600, function () {
+                        dice.removeClass("dice_s").addClass("dice_e");
+                    }).delay(100).animate({left: '-2px', top: '2px'}, 100, function () {
+                        dice.removeClass("dice_e").addClass("dice_" + num);
+                        $("#result").html("Your throwing points are<span>" + num + "</span>");
+                        dice.css('cursor', 'pointer');
+                        $("#dice_mask").remove();//remove mask
+                    });
+                });
+            });
         }
     };
 });
